d90b0b41efd76cabc87abaf78febd6f41564e972
Sam Vilain <sam@vilain.net>
2014-06-08 22:37:44 -0700

    Looks like JUnit has assertEquals(this, that) the other way around
    Python and Perl are the other way around.

--- tests/ninja/tipjar/TipJarTest.java
+++ tests/ninja/tipjar/TipJarTest.java
@@ -17,13 +17,13 @@ public class TipJarTest {
     private void assertModelState(double subAmount, double totalAmount,
                                   double taxPercent, double taxAmount,
                                   double tipPercent, double tipAmount) {
-        assertEquals("Subtotal", bill.getSubAmount(), subAmount, 0.001d);
-        assertEquals("Tax %", bill.getTaxPercent(), taxPercent, 0.001d);
-        assertEquals("Tip %", bill.getTipPercent(), tipPercent, 0.001d);
+        assertEquals("Subtotal", subAmount, bill.getSubAmount(), 0.001d);
+        assertEquals("Tax %", taxPercent, bill.getTaxPercent(), 0.001d);
+        assertEquals("Tip %", tipPercent, bill.getTipPercent(), 0.001d);
 
-        assertEquals("Tax Amount", bill.getTaxAmount(), taxAmount, 0.001d);
-        assertEquals("Tip Amount", bill.getTipAmount(), tipAmount, 0.001d);
-        assertEquals("Total Amount", bill.getTotalAmount(), totalAmount, 0.001d);
+        assertEquals("Tax Amount", taxAmount, bill.getTaxAmount(), 0.001d);
+        assertEquals("Tip Amount", tipAmount, bill.getTipAmount(), 0.001d);
+        assertEquals("Total Amount", totalAmount, bill.getTotalAmount(), 0.001d);
     }
 
     @Test
